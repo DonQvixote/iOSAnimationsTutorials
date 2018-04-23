3b067ff3c57f8816cbcfabf3f1183e99e2c5bf64
Cold Cat <yuchengzhixia@gmail.com>
2018-04-23 15:09:19 +0800

    Getting Started with View Animations

---
+++ Bahama/Bahama/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Bahama
+//
+//  Created by 夏语诚 on 2018/4/23.
+//  Copyright © 2018年 Banana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - IB outlets
+    
+    @IBOutlet weak var heading: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var cloud1: UIImageView!
+    @IBOutlet weak var cloud2: UIImageView!
+    @IBOutlet weak var cloud3: UIImageView!
+    @IBOutlet weak var cloud4: UIImageView!
+    
+    // MARK: - IB actions
+    
+    @IBAction func login() {
+        view.endEditing(true)
+    }
+    
+    // MARK: - ViewController methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup UI
+        loginButton.layer.cornerRadius = 8.0
+        loginButton.layer.masksToBounds = true
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        heading.center.x -= view.bounds.width
+        usernameTextField.center.x -= view.bounds.width
+        passwordTextField.center.x -= view.bounds.width
+        
+        cloud1.alpha = 0.0
+        cloud2.alpha = 0.0
+        cloud3.alpha = 0.0
+        cloud4.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.heading.center.x += self.view.bounds.width
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: [], animations: {
+            self.usernameTextField.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
+            self.passwordTextField.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
+            self.cloud1.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: [], animations: {
+            self.cloud2.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: [], animations: {
+            self.cloud3.alpha = 1.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
+            self.cloud4.alpha = 1.0
+        }, completion: nil)
+    }
+
+}
+
