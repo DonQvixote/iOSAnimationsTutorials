353d17885fee1472047018b3ae61e8da3c83c23f
Cold Cat <yuchengzhixia@gmail.com>
2018-04-23 15:41:21 +0800

    Springs

--- Bahama/Bahama/ViewController.swift
+++ Bahama/Bahama/ViewController.swift
@@ -26,7 +26,23 @@ class ViewController: UIViewController {
     
     @IBAction func login() {
         view.endEditing(true)
+        
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.bounds.size.width += 80.0
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.center.y += 60.0
+            self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+            
+            self.spinner.center = CGPoint(x: 40.0, y: self.loginButton.frame.size.height / 2)
+            self.spinner.alpha = 1.0
+        }, completion: nil)
     }
+    
+    // MARK: - futher UI
+    
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
     
     // MARK: - ViewController methods
     
@@ -37,6 +53,11 @@ class ViewController: UIViewController {
         // Setup UI
         loginButton.layer.cornerRadius = 8.0
         loginButton.layer.masksToBounds = true
+        
+        spinner.frame = CGRect(x: -20.0, y: 6.0, width: 20.0, height: 20.0)
+        spinner.alpha = 0.0
+        spinner.startAnimating()
+        loginButton.addSubview(spinner)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -50,6 +71,9 @@ class ViewController: UIViewController {
         cloud2.alpha = 0.0
         cloud3.alpha = 0.0
         cloud4.alpha = 0.0
+        
+        loginButton.center.y += 30.0
+        loginButton.alpha = 0.0
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -81,6 +105,11 @@ class ViewController: UIViewController {
         
         UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
             self.cloud4.alpha = 1.0
+        }, completion: nil)
+    
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.center.y -= 30.0
+            self.loginButton.alpha = 1.0
         }, completion: nil)
     }
 
